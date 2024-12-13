97ce1f594743fe7e6072b05ca0af5c50e2720e6b
anushastefyj <24hr5a3001@mtieat.org>
2024-12-13 14:14:25 +0530

    Add files via upload

---
+++ modify.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class modify
+{
+    public static void main(String args[])
+    {
+    HashMap<String,String> a=new HashMap<>();
+    a.put("SNO","01");
+    a.put("Sname","anu");
+    System.out.println(a);
+    //modify
+    a.put("Sno","90");
+    System.out.println(a);
+    }
+}
