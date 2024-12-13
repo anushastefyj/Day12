18f0f7ceeb318b276256f392b38cd5f26d279048
anushastefyj <24hr5a3001@mtieat.org>
2024-12-13 13:49:18 +0530

    Add files via upload

---
+++ remove.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class remove
+{
+    public static void main(String args[]){
+        // remove
+        LinkedHashSet<String> a=new LinkedHashSet<>();
+        a.add("HELLo");
+        a.add("HELLo");
+        a.add("students");
+        a.add("rajesh");
+        a.add("students");
+        a.remove("students");
+        }
+    }
